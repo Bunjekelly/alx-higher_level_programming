9d669467d4fbda2cf64f3a6f00a7d1a36e4c8787
Bunje Kelly <kellybunje@gmail.com>
2023-06-07 12:37:18 -0700

    adding a number to singly linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts a number into singly linked list
+ * @head: pointer to head
+ * @number: number to insert
+ *
+ * Return: pointer to new node or NULL if it fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
