fc88f698b6681591ab96a84402c2488e40f5d63d
Bunje Kelly <kellybunje@gmail.com>
2023-06-27 13:37:52 -0700

    a class Square that defines a square by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+"""class Square that defines a square by size"""
+
+
+class Square:
+    """the square"""
+    def __init__(self, size):
+        """Initializes the class square with a
+        private attribute size"""
+        self.__size = size
