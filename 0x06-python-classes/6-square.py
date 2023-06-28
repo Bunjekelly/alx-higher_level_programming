dc05afeb70a2a3d61d432d7d5c5eabffc7490c83
Bunje Kelly <kellybunje@gmail.com>
2023-06-28 04:12:11 -0700

    square class with size, position tuple, area

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,14 +33,13 @@ class Square:
             print("")
             return
         for i in range(0, self.__position[1]):
-                print("")
+            print("")
         for i in range(self.__size):
             for j in range(0, self.__position[0]):
                 print(" ", end="")
             for k in range(0, self.__size):
                 print("#", end="")
             print("")
-
 
     @property
     def size(self):
