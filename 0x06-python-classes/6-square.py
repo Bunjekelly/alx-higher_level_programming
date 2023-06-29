dab5179800d98e27c367b52985cd92d9d1594615
Bunje Kelly <kellybunje@gmail.com>
2023-06-29 03:01:59 -0700

    class square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
             raise ValueError('size must be >= 0')
 
         self.__size = size
-        
+
         if not isinstance(position, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
         if len(position) != 2:
