c0514c3f48d2a16f1478acfc24696fa72e7ce5fe
Bunje Kelly <kellybunje@gmail.com>
2023-07-02 07:41:42 -0700

    creating a doctest file for a function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# 0-add_integer.py
+"""a function that adds 2 integers"""
+
+def add_integer(a, b=98):
+    """accepts integers or float values
+    and returns the sum of integer addition"""
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer or float")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer or float")
+    return (int(a) + int(b))
