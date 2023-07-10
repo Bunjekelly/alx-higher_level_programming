5c45292e7603b07e022c59579a7331409e98bc87
Bunje Kelly <kellybunje@gmail.com>
2023-07-10 10:46:53 -0700

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+
+"""a class MyList that inherits from list"""
+
+class MyList(list):
+    """defining a class MyList that inherits from base class list"""
+
+    def print_sorted(self):
+        """public instance method that prints the sorted list"""
+        print(sorted(self))
