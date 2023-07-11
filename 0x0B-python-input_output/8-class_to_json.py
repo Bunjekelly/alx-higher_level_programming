e1105b0602188e65a8020bcb9b77fd3e3bcc87af
Bunje Kelly <kellybunje@gmail.com>
2023-07-11 14:00:11 -0700

    converting class to json dict representation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,8 +4,6 @@
 data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object"""
 
-import json
-
 
 def class_to_json(obj):
     """returns the dictionary representation of an object"""
