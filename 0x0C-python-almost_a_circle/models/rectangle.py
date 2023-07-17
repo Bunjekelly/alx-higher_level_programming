be64582637c22f2c20d76b46699177027c2f191f
Bunje Kelly <kellybunje@gmail.com>
2023-07-17 12:07:41 -0700

    try

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,8 +111,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} -
-                {self.width}/{self.height}")
+        v = f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return v
 
     def update(self, *args, **kwargs):
         """assigns an argument to each attribute"""
