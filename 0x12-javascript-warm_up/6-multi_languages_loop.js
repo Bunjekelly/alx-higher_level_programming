cfa49daba874858100ef337050afd27a64cca44b
Bunje Kelly <kellybunje@gmail.com>
2023-09-13 00:30:37 -0700

    loop and print

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,6 @@
 
 const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let output = '';
 for (let i = 0; i < 3; i++) {
-  output += strings[i] + '\n';
+  console.log(strings[i]);
 }
-
-console.log(output);
