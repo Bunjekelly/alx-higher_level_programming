0ddd800cde6494ef891792d8dc979a249a2d280d
Bunje Kelly <kellybunje@gmail.com>
2023-09-30 20:46:21 -0700

    advanced

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes 2 arguments in order to
+list 10 most recent commits for a github user"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    response = requests.get(url)
+    commits = response.json()
+
+    for i in range(10):
+        print("{}: {}".format(commits[i]['sha'],
+                              commits[i]['commit']['author']['name']))
