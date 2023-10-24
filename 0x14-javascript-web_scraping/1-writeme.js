05fd33a7ea175942141830eab40125f4d43e0cf5
Bunje Kelly <kellybunje@gmail.com>
2023-10-24 14:07:52 -0700

    writing a string to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filepath, string, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+});
