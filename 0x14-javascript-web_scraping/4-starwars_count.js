1054f1ebd6f870edbefd94f7c433c845ab4ceb09
Bunje Kelly <kellybunje@gmail.com>
2023-10-25 05:36:53 -0700

    starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiurl = process.argv[2];
+
+request(apiurl, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body).results;
+
+    const Movies = data.filter(movie =>
+      movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+
+    console.log(Movies.length);
+  }
+});
